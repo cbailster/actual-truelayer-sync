632ff4beceec7ec2680ec22bbb4d55481044d0b3
Chris Bailey <shared@cmbwebsites.com>
2026-07-22 11:46:40 +0100

    Force insecure cookies

--- src/web/index.ts
+++ src/web/index.ts
@@ -74,7 +74,10 @@ const buildApp = async (fastify: import('fastify').FastifyInstance) => {
   await fastify.register(fastifyCookie)
   await fastify.register(fastifySession, {
     // TODO: Move this to an environment variable for production
-    secret: process.env.SESSION_SECRET || 'a-very-long-and-super-secret-key-for-sessions'
+    secret: process.env.SESSION_SECRET || 'a-very-long-and-super-secret-key-for-sessions',
+    cookie: { 
+      secure: false
+    },
   })
 
   // Decorate fastify with the config and a method to reload it
